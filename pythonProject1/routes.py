aed2748511a8bd7d0d94db424568dea5a5b3f762
Evg <89110868071@mail.ru>
2024-10-11 16:28:32 +0300

    работают все три метода, но get заменен на put, что вроде как не круто

--- pythonProject1/routes.py
+++ pythonProject1/routes.py
@@ -9,7 +9,7 @@ api_router = APIRouter()
 @api_router.post("/me")
 def set_my_data(input_data: TrainSchemaPeople,
                 db: Session = Depends(get_db)):
-    if not (db.query(User).filter(User.surname == input_data.surname).first() and db.query(User).filter(User.name == input_data.name).first()):
+    if not (db.query(User).filter(User.surname == input_data.surname and User.name == input_data.name).first()):
         user = User()
         user.name = input_data.name
         user.surname = input_data.surname
@@ -22,30 +22,28 @@ def set_my_data(input_data: TrainSchemaPeople,
     else:
         return {"message": "Пользователь с такими именем и фамилией уже существует"}
 
-# @api_router.get("/me")
-# def get_my_data(name: str,
-#                 db: Session = Depends(get_db)):
-#     user = db.query(User).filter(User.name == name).first()
-#     # user = db.query(User).filter(User.name == input_name.name).first()
-#     # if user:
-#     #     return {"name": user.name,
-#     #         "surname": user.surname,
-#     #         "age": user.age}
-#     # else:
-#     #     return {"message": "Пользователь с таким именем не найден"}
-#     return {"name": name}
+@api_router.put("/me")
+def get_my_data(input_data: TrainSchemaName,
+                db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.name == input_data.name).first()
+    if user:
+        return {"name": user.name,
+            "surname": user.surname,
+            "age": user.age}
+    else:
+        return {"message": "Пользователь с таким именем не найден"}
 
 
-# @api_router.delete("/me")
-# def delete_my_data(input_name: TrainSchemaName,
-#                    db: Session = Depends(get_db)):
-#     user = db.query(User).filter(User.name == input_name.name).first()
-#     if user:
-#         db.delete(user)
-#         db.commit()
-#         return {"message ": "пользователь удален"}
-#     else:
-#         return {"message ": "пользователь не найден"}
+@api_router.delete("/me")
+def delete_my_data(input_name: TrainSchemaName,
+                   db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.name == input_name.name).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return {"message ": "удален первый пользователь с таким именем "}
+    else:
+        return {"message ": "пользователь не найден"}
 
 
 
